6cf583fc5d49b270f8a12f13808216175f373f44
CIKADA <adel.garipovazn@mail.ru>
2023-02-23 18:35:45 +0300

    Init project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const App = {
+  data() {
+    return {
+      title: "Programmer memes registration",
+      subTitle: "Form registration for mem's author",
+    };
+  },
+};
+
+const app = Vue.createApp(App);
+
+app.mount("#app");
